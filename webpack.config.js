33c3769ba325d0caa03ed30d730cd00003220199
Jan Steikowski <j.steikowski@aol.com>
2023-04-17 13:57:47 +0200

    Set up linter, prettier etc

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
+  plugins: [
+    new HtmlWebPackPlugin({
+      title: 'Development',
+    }),
+  ],
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clear: true,
   },
 };
